19650ec561101f4584219ac3c72a28468f8e403f
Daniel <daniel.j.fletcher2@gmail.com>
2017-07-13 15:29:37 -0700

    document up and running

---
+++ recent.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let root = document.getElementById("root");
+  let test = document.createElement("div");
+  test.innerHTML = "I AM TEST";
+  root.appendChild(test);
+});
