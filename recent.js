15529871cce6ebf1f11cae88758bc75de8ed8710
Daniel <daniel.j.fletcher2@gmail.com>
2017-07-13 15:40:30 -0700

    Write api request functions for posts and comments

--- recent.js
+++ recent.js
@@ -3,4 +3,33 @@ document.addEventListener("DOMContentLoaded", () => {
   let test = document.createElement("div");
   test.innerHTML = "I AM TEST";
   root.appendChild(test);
+  let posts = getPosts('kijafa').then(posts => console.log(posts));
+  let comments = getComments('kijafa').then(comments => console.log(comments));
 });
+
+
+// Reddit API requests
+
+const fetchPosts = username => (
+  $.ajax({
+    method: 'GET',
+    url: `https://www.reddit.com/user/${username}/submitted.json`
+  })
+);
+
+const fetchComments = username => (
+  $.ajax({
+    method: 'GET',
+    url: `https://www.reddit.com/user/${username}/comments.json`
+  })
+);
+
+// Parsing API response
+
+const getPosts = username => {
+  return fetchPosts(username).then(data => data["data"]["children"]);
+};
+
+const getComments = username => {
+  return fetchPosts(username).then(data => data["data"]["children"]);
+};
